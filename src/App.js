6244771e9c1a6bc0934cd74de82d6f522cfe2444
aqsafaisal34 <aqsay.faisal34@gmail.com>
2022-12-13 00:24:33 +0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import  Prayer from './prayer';
+
+
+import axios from 'axios';
 
 function App() {
+  const [location,setLocation]= useState ("");
+  const [data,setData]=useState([]);
+
+  let submitHandler = async (e) => {
+    e.preventDefault();
+    console.log("I am submit Handler")
+
+
+    try {
+      let response = await axios.get(`http://api.aladhan.com/v1/calendarByCity?city=${location}&country=pakistan&method=2&month=04&year=2017appid=e380935c24msh941f6b3907949b0p1f070bjsn6eff51300597`)
+
+      // http://api.aladhan.com/v1/calendarByCity?city=${location}&country=pakistan&method=2&month=04&year=2017appid=e380935c24msh941f6b3907949b0p1f070bjsn6eff51300597
+
+      console.log("response: ", response);
+
+      setData(response.data.list)
+
+
+  } catch (error) {
+      console.log("error in api call: ", error);
+  }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Namaz App</h1>
+      <form onSubmit={submitHandler}>
+        <label htmlFor='labelInput'></label> <br />
+        <input
+         type="text" 
+         required
+         placeholder='Enter your location'
+         onChange={(e)=>{
+          setLocation(e.target.value);
+         }}
+        />
+        <button type='submit'>Check</button>
+      </form>
+      {
+        data.map((eachForecast, index)=>(
+          <Prayer
+          key={index}
+          time={eachForecast.data.data.timings.Fajr}
+           />
+        ))
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/prayer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let Prayer = ({time}) => {
+    return(
+        <div>
+            <p>Fajr{time}</p>
+        </div>
+    )
+}
+
+export default Prayer;
